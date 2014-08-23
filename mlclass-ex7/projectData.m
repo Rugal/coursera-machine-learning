b0a33ad11457145683af706d3d46efad1f4fc540
Rugal Bernstein <ryujinwrath@gmail.com>
2014-08-22 20:25:12 -0500

    projectdata

--- mlclass-ex7/projectData.m
+++ mlclass-ex7/projectData.m
@@ -14,12 +14,10 @@ Z = zeros(size(X, 1), K);
 %               eigenvectors in U (first K columns). 
 %               For the i-th example X(i,:), the projection on to the k-th 
 %               eigenvector is given as follows:
-%                    x = X(i, :)';
-%                    projection_k = x' * U(:, k);
 %
 
-
-
+ur=U(:,1:K);
+Z=X*ur;
 
 % =============================================================
 
